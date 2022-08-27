4cb81a1fa216b25ffe3f2a7881ff1631a10f9a5f
Yasmin Askar <hailey.askar@gmail.com>
2022-08-27 11:59:38 -0500

    static setup

--- test/src/App.js
+++ test/src/App.js
@@ -4,6 +4,13 @@ import "./App.css";
 import SearchIcon from "./search.svg";
 
 const API_URL = "http://www.omdbapi.com?apikey=35a5cb42";
+const movie1 = {
+  Title: "Hollywood's Master Storytellers: Spiderman Live",
+  Year: "2006",
+  imdbID: "tt2158533",
+  Type: "movie",
+  Poster: "N/A",
+};
 const App = () => {
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
@@ -28,7 +35,28 @@ const App = () => {
         <img src={SearchIcon} alt="search" onClick={() => {}} />
       </div>
 
-      <div className="container"></div>
+      <div className="container">
+        <div className="movie">
+          <div>
+            <p>{movie1.Year}</p>
+          </div>
+          <div>
+            <img
+              src={
+                movie1.Poster !== "N/A"
+                  ? movie1.Poster
+                  : "https://via.placeholder.com/400"
+              }
+              alt={movie1.Title}
+            />
+          </div>
+
+          <div>
+            <span>{movie1.Type}</span>
+            <h3>{movie1.Title}</h3>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
